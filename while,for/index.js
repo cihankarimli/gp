26b08bd8bdcf959fe19845b7f98eb4316ddcab4b
cihankarimli <kerimlicihan1@gmail.com>
2025-01-18 18:02:46 +0400

    elave edildi

---
+++ while,for/index.js
@@ -0,0 +1,113 @@
+//1 ci tapsiriq
+
+/*let firstNumber=Number(prompt('1ci eded'))
+let secondNumber=Number(prompt('2ci eded'))
+let thirdNumber=Number(prompt('3cu eded'))
+if (firstNumber>secondNumber && firstNumber>thirdNumber){
+    alert(firstNumber)
+}else if(secondNumber>firstNumber && secondNumber>thirdNumber){
+    alert(secondNumber)
+
+}else{
+    alert(thirdNumber)
+}*/
+
+//2 ci tapsiriq
+/*let year=prompt('il daxil edin')
+if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    console.log("Artiq il");
+}else{
+     alert('Artiq il deyil')
+
+}*/
+     
+
+
+ //3 cu tapsiriq
+
+
+/*let point=Number(prompt('sagirdin bali',''))
+if(point>90 && point<100){
+    alert('A')
+}else if(80<point && point<=90){
+    alert('B')
+}else if(70<point && point<=80){
+    alert('C')
+}else if(60<point && point<=70){
+    alert('D')
+}else if(50<point && point<=60){
+    alert('E')
+}else if(50>point){
+    alert('Kesildin')
+}else{
+    alert('bele bir bal yoxdur')
+}*/
+
+//4 cu tapsiriq
+
+/*let number=Number(prompt('eded daxil edin'))
+if(number%2==0){
+alert('Bu eded cut ededdir')
+}else{
+    alert('Bu eded tek ededdir')
+}*/
+
+//5 ci tapsiriq
+
+
+/*for(let i=1;i<=50;i++){
+    if(i%3==0 && i%5==0){
+        console.log('FizzBuzz',i)
+    }
+    else if(i%3==0){
+        console.log('Fizz',i)
+    }else if(i%5==0){
+        console.log('Buzz',i)
+    }
+}*/
+
+//6 ci tapsiriq
+
+/*let number=Number(prompt('Eded daxil edin'))
+for(let i=1;i<=10;i++){
+    let vurma=number*i
+    console.log(number,'*',i,'=',vurma)
+}*/
+
+//7 ci tapsiriq
+
+/*let number=Number(prompt('Bir eded daxil edin'))
+let sum=0
+for(let i=0;i<number;i++){
+    sum=sum+i
+}
+alert(sum)*/
+
+//8 ci tapsiriq
+
+
+/*let operator=prompt('Oparotoru daxil edin')
+let num1=Number(prompt('! ci ededi daxil edin'))
+let num2=Number(prompt('2 ci ededi daxil edin'))
+switch (operator) {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        if (num2 !== 0) {
+            result = num1 / num2;
+        } else {
+            result = 'Sifira bolmek olmaz';
+        }
+        break;
+    default:
+        result = 'Yanlis oparotor secilib';
+}
+     alert(result)*/
+
