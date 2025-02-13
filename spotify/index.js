ea483790637593b2d4b90cef50f80c3d365b30b6
cihankarimli <kerimlicihan1@gmail.com>
2025-02-13 15:59:03 +0400

    elave edildi

--- spotify/index.js
+++ spotify/index.js
@@ -2,6 +2,7 @@
 let allButton=document.getElementById('All')
 let cards = document.querySelectorAll('.card')
 let buttons=document.querySelectorAll('.button-card')
+let h3=document.querySelectorAll('h3')
 
 allButton.addEventListener('click',function () {
     cards.forEach(card => {
@@ -16,7 +17,6 @@ for (let i = 1; i < buttons.length; i++) {
     });
   
    
-     let h3=document.querySelectorAll('h3')
      for (let f = 0; f< h3.length; f++) {
         if (buttons[i].textContent===h3[f].textContent) {
           cards[f].style.display = 'block';
@@ -25,3 +25,38 @@ for (let i = 1; i < buttons.length; i++) {
      }   
  });
    }
+let plus=document.querySelectorAll('.plus')
+let cardTitle=document.querySelectorAll('card-title')
+let listCardsimg=document.querySelectorAll('.playlist-img')
+let cardImg=document.querySelectorAll('.card-img')
+
+for (let i = 0; i < listCardsimg.length; i++) {
+  for (let f = 0; f < listCardsimg.length; f++) {
+  
+    plus[i].addEventListener('click',function () {
+    
+      listCardsimg[i].src=cardImg[i].src
+      
+      
+    })
+  }
+
+
+}
+
+  
+
+
+
+
+
+
+
+
+
+    
+            
+
+
+
+
