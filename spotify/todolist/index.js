6e113ed528b313c05b0cbf518ec32e3b08fa157e
cihankarimli <kerimlicihan1@gmail.com>
2025-02-19 18:03:02 +0400

    deyisdirildi

--- spotify/todolist/index.js
+++ spotify/todolist/index.js
@@ -24,6 +24,9 @@
             todoList.appendChild(listItem);
             input.value = ''; 
         }
+        else{
+            alert('Melumati daxil edin')
+        }
         
        
        
